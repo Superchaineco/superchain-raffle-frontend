c1bf947c900951fefc241fb476b0072d82f717bb
AnotherDev <joseluismanco37@gmail.com>
2024-08-29 17:57:45 -0300

    feat: add a hook to fetch the user SCSA

---
+++ src/constants/index.tsx
@@ -0,0 +1,3 @@
+import superchainModuleABI from './abi/SCSAModule.abi.json'
+export const SUPER_CHAIN_ACCOUNT_MODULE_ADDRESS = '0x48D64d3f2B43f68d3F26384809e18bF90E4F2a31'
+export const SUPER_CHAIN_ACCOUNT_MODULE_ABI = superchainModuleABI
---
+++ src/hooks/useGetSuperchainAccount.ts
@@ -0,0 +1,99 @@
+import { Address, createPublicClient, http } from 'viem'
+import { useQuery } from 'react-query'
+import { sepolia } from 'viem/chains'
+import { SUPER_CHAIN_ACCOUNT_MODULE_ADDRESS } from '@/constants'
+
+export type SuperChainAccount = {
+    smartAccount: Address
+    superChainID: string
+    points: bigint
+    level: number
+    noun: {
+        background: number
+        body: number
+        accessory: number
+        head: number
+        glasses: number
+    }
+}
+
+function useGetSuperchainAccount(address: Address) {
+    return useQuery<SuperChainAccount>({
+        queryKey: ['superChainAccount', address],
+        queryFn: async () => {
+            const publicClient = createPublicClient({
+                chain: sepolia,
+                transport: http(),
+            })
+
+            const response = await publicClient.readContract({
+                address: SUPER_CHAIN_ACCOUNT_MODULE_ADDRESS,
+                abi: [{
+                    "type": "function",
+                    "name": "getSuperChainAccount",
+                    "inputs": [
+                        { "name": "_safe", "type": "address", "internalType": "address" }
+                    ],
+                    "outputs": [
+                        {
+                            "name": "",
+                            "type": "tuple",
+                            "internalType": "struct ISuperChainModule.Account",
+                            "components": [
+                                {
+                                    "name": "smartAccount",
+                                    "type": "address",
+                                    "internalType": "address"
+                                },
+                                {
+                                    "name": "superChainID",
+                                    "type": "string",
+                                    "internalType": "string"
+                                },
+                                { "name": "points", "type": "uint256", "internalType": "uint256" },
+                                { "name": "level", "type": "uint16", "internalType": "uint16" },
+                                {
+                                    "name": "noun",
+                                    "type": "tuple",
+                                    "internalType": "struct NounMetadata",
+                                    "components": [
+                                        {
+                                            "name": "background",
+                                            "type": "uint48",
+                                            "internalType": "uint48"
+                                        },
+                                        { "name": "body", "type": "uint48", "internalType": "uint48" },
+                                        {
+                                            "name": "accessory",
+                                            "type": "uint48",
+                                            "internalType": "uint48"
+                                        },
+                                        { "name": "head", "type": "uint48", "internalType": "uint48" },
+                                        {
+                                            "name": "glasses",
+                                            "type": "uint48",
+                                            "internalType": "uint48"
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    ],
+                    "stateMutability": "view"
+                }] as const,
+                functionName: 'getSuperChainAccount',
+                args: [address],
+            })
+            return {
+                smartAccount: response.smartAccount,
+                superChainID: response.superChainID,
+                points: response.points,
+                level: response.level,
+                noun: response.noun,
+            }
+        },
+    })
+
+}
+
+export default useGetSuperchainAccount
