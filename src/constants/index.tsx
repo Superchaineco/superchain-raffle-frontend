2f8c6ebc5f5dfbabeb39c10722c2cf5fd86c8a31
anotherdev <joseluismanco37@gmail.com>
2024-09-30 17:23:24 -0500

    feat(production): update SC

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -1,7 +1,7 @@
 import superchainModuleABI from "./abi/SCSAModule.abi.json";
 import superchainRaffleABI from "./abi/SCRaffle.abi.json";
 export const SUPER_CHAIN_ACCOUNT_MODULE_ADDRESS =
-  "0x37e4783e5AfE03A49520c48e103683574447a81f";
-export const SUPER_CHAIN_RAFFLE = "0x7997454073d9e80fd0b6FcD0308B9068C7522448";
+  "0x1Ee397850c3CA629d965453B3cF102E9A8806Ded";
+export const SUPER_CHAIN_RAFFLE = "0x39987445BC31823c8BE2d0AA4577CA44d7233A6F";
 export const SUPER_CHAIN_ACCOUNT_MODULE_ABI = superchainModuleABI;
 export const SUPER_CHAIN_RAFFLE_ABI = superchainRaffleABI;
