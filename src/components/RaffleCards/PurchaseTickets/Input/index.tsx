68534a84bbd75948c958207eb90e7633b60cf1b0
anotherdev <joseluismanco37@gmail.com>
2024-10-02 13:29:08 -0500

    fix: arrows

--- src/components/RaffleCards/PurchaseTickets/Input/index.tsx
+++ src/components/RaffleCards/PurchaseTickets/Input/index.tsx
@@ -1,32 +1,33 @@
-import React, { useContext, useState } from "react";
+import React, { ChangeEvent, useContext, useState } from 'react';
 import {
   InputAdornment,
   Stack,
   SvgIcon,
   TextField,
   Typography,
-} from "@mui/material";
-import ArrowUpIcon from "@/public/images/arrow-up-icon.svg";
-import ArrowDownIcon from "@/public/images/arrow-down-icon.svg";
-import styles from "./styles.module.css";
-import ActionModalContentTicketsInfo from "@/components/ActionModal/Content/Tickets";
-import { ActionModalContext } from "@/views/DashBoard";
-import { TicketsContext } from "../../Raffle";
-import { useSafeAppsSDK } from "@safe-global/safe-apps-react-sdk";
-import { Interface } from "ethers";
-import { SUPER_CHAIN_RAFFLE } from "@/constants";
-import { ActionModalStatus } from "@/types/commons";
+} from '@mui/material';
+import ArrowUpIcon from '@/public/images/arrow-up-icon.svg';
+import ArrowDownIcon from '@/public/images/arrow-down-icon.svg';
+import styles from './styles.module.css';
+import ActionModalContentTicketsInfo from '@/components/ActionModal/Content/Tickets';
+import { ActionModalContext } from '@/views/DashBoard';
+import { TicketsContext } from '../../Raffle';
+import { useSafeAppsSDK } from '@safe-global/safe-apps-react-sdk';
+import { Interface } from 'ethers';
+import { SUPER_CHAIN_RAFFLE } from '@/constants';
+import { ActionModalStatus } from '@/types/commons';
 
 export default function PurchaseTicketsInput({ max }: { max: number }) {
   const { sdk, safe } = useSafeAppsSDK();
-  const [quantity, setQuantity] = useState<number | null>(null);
+  const [quantity, setQuantity] = useState<number>(0);
   const { actionModalContextState, setActionModalContextState } =
     useContext(ActionModalContext);
 
   const onBuyTickets = async () => {
+    if (!quantity || quantity <= 0 || quantity > max) return;
     setActionModalContextState({
       open: true,
-      title: "Confirm to Claim Your Rewards",
+      title: 'Confirm to Claim Your Rewards',
       loadComponent: <></>,
       contentComponent: <></>,
       status: ActionModalStatus.LOADING,
@@ -34,27 +35,27 @@ export default function PurchaseTicketsInput({ max }: { max: number }) {
     try {
       const iface = new Interface([
         {
-          type: "function",
-          name: "enterRaffle",
+          type: 'function',
+          name: 'enterRaffle',
           inputs: [
             {
-              name: "_numberOfTickets",
-              type: "uint256",
-              internalType: "uint256",
+              name: '_numberOfTickets',
+              type: 'uint256',
+              internalType: 'uint256',
             },
           ],
           outputs: [],
-          stateMutability: "nonpayable",
+          stateMutability: 'nonpayable',
         },
       ]);
-      const calldata = iface.encodeFunctionData("enterRaffle", [
-        BigInt(quantity || 0)
+      const calldata = iface.encodeFunctionData('enterRaffle', [
+        BigInt(quantity), 
       ]);
 
       const txs = [
         {
           to: SUPER_CHAIN_RAFFLE,
-          value: "0",
+          value: '0',
           data: calldata,
         },
       ];
@@ -62,7 +63,7 @@ export default function PurchaseTicketsInput({ max }: { max: number }) {
       let transactionConfirmed = false;
       while (!transactionConfirmed) {
         const status = await sdk.txs.getBySafeTxHash(transaction.safeTxHash);
-        if (status.txStatus == "SUCCESS") {
+        if (status.txStatus == 'SUCCESS') {
           transactionConfirmed = true;
         } else {
           await new Promise((resolve) => setTimeout(resolve, 5000));
@@ -87,26 +88,25 @@ export default function PurchaseTicketsInput({ max }: { max: number }) {
   };
 
   const increaseQuantity = () => {
-    if (quantity && quantity <= max) {
+    if (quantity < max) {
       setQuantity(quantity + 1);
     }
   };
 
   const decreaseQuantity = () => {
-    if (quantity && quantity >= 0) {
+    if (quantity > 0) {
       setQuantity(quantity - 1);
     }
   };
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.value === "" || event.target.value === null)
-      setQuantity(null);
     const value = Number(event.target.value);
     if (isNaN(value)) return;
+
     if (value > max) {
       setQuantity(max);
     } else if (value < 0) {
-      setQuantity(null);
+      setQuantity(0);
     } else {
       setQuantity(value);
     }
@@ -117,49 +117,50 @@ export default function PurchaseTicketsInput({ max }: { max: number }) {
       style={{
         opacity: quantity && quantity <= max ? 1 : 0.5,
       }}
-      direction={"row"}
-      alignItems="stretch"
-      justifyContent={"center"}
+      direction={'row'}
+      alignItems='stretch'
+      justifyContent={'center'}
     >
       <TextField
-        className={styles["input--buy"]}
+        className={styles['input--buy']}
         value={quantity}
-        size="small"
-        inputProps={{ inputMode: "numeric", min: 0, max: max }}
-        placeholder="0"
-        style={{ borderRadius: "0px 6px 6px 0px" }}
+        size='small'
+        inputProps={{ inputMode: 'numeric', min: 0, max: max }}
+        placeholder='0'
+        style={{ borderRadius: '0px 6px 6px 0px' }}
         onChange={handleInputChange}
         InputProps={{
           endAdornment: (
-            <InputAdornment position="end">
+            <InputAdornment position='end'>
               <Stack
-                direction={"row"}
+                direction={'row'}
                 spacing={1}
-                alignItems={"center"}
-                justifyContent={"center"}
+                alignItems={'center'}
+                justifyContent={'center'}
               >
                 <button
                   style={{
-                    backgroundColor: "transparent",
-                    border: "none",
-                    cursor: "pointer",
+                    backgroundColor: 'transparent',
+                    border: 'none',
+                    cursor: 'pointer',
                     opacity: max ? 1 : 0.5,
                   }}
                   onClick={() => setQuantity(max)}
                   disabled={!max}
                 >
-                  <Typography className={styles["decorator--max"]}>
+                  <Typography className={styles['decorator--max']}>
                     Max
                   </Typography>
                 </button>
-                <Stack direction={"column"} spacing={1}>
+                <Stack direction={'column'} spacing={1}>
                   <SvgIcon
                     onClick={increaseQuantity}
                     component={ArrowUpIcon}
                     inheritViewBox
                     style={{
-                      width: "8px",
-                      height: "8px",
+                      cursor: 'pointer',
+                      width: '8px',
+                      height: '8px',
                     }}
                   />
                   <SvgIcon
@@ -167,8 +168,9 @@ export default function PurchaseTicketsInput({ max }: { max: number }) {
                     component={ArrowDownIcon}
                     inheritViewBox
                     style={{
-                      width: "8px",
-                      height: "8px",
+                      cursor: 'pointer',
+                      width: '8px',
+                      height: '8px',
                     }}
                   />
                 </Stack>
@@ -179,11 +181,11 @@ export default function PurchaseTicketsInput({ max }: { max: number }) {
       />
       <button
         style={{
-          cursor: "pointer",
+          cursor: 'pointer',
           opacity: quantity && quantity <= max ? 1 : 0.5,
         }}
         onClick={onBuyTickets}
-        className={styles["button--buy"]}
+        className={styles['button--buy']}
         disabled={!quantity}
       >
         Claim
