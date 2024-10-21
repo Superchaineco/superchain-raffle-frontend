06ad400ea691ea054049738efb6d5dd6119c3758
anotherdev <joseluismanco37@gmail.com>
2024-10-21 14:53:06 -0500

    feat: upgrade contracts

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -2,6 +2,6 @@ import superchainModuleABI from "./abi/SCSAModule.abi.json";
 import superchainRaffleABI from "./abi/SCRaffle.abi.json";
 export const SUPER_CHAIN_ACCOUNT_MODULE_ADDRESS =
   "0x1Ee397850c3CA629d965453B3cF102E9A8806Ded";
-export const SUPER_CHAIN_RAFFLE = "0x39987445BC31823c8BE2d0AA4577CA44d7233A6F";
+export const SUPER_CHAIN_RAFFLE = "0x30B6f7C268fa02b96284A7A2b3Af38E006b5e2A2";
 export const SUPER_CHAIN_ACCOUNT_MODULE_ABI = superchainModuleABI;
 export const SUPER_CHAIN_RAFFLE_ABI = superchainRaffleABI;
